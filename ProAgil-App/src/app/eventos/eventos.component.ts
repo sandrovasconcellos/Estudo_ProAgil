6056003bfcbf39a749aa11916d9d9f751f667a8a
Sandro Vasconcellos dos Santos <sandro.vasconcellos@gmail.com>
2020-05-14 18:51:21 -0300

    teste

--- ProAgil-App/src/app/eventos/eventos.component.ts
+++ ProAgil-App/src/app/eventos/eventos.component.ts
@@ -1,35 +1,187 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { EventoService } from '../_services/evento.service';
+import { Evento } from '../_models/Evento';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { BsLocaleService} from 'ngx-bootstrap/datepicker';
+import { defineLocale } from 'ngx-bootstrap/chronos';
+import { ptBrLocale } from 'ngx-bootstrap/chronos';
 
+defineLocale('pt-br', ptBrLocale);
+
+// decoreitor
 @Component({
   selector: 'app-eventos',
   templateUrl: './eventos.component.html',
   styleUrls: ['./eventos.component.css']
 })
+
 export class EventosComponent implements OnInit {
-  eventos: any = [];
+  // variaveis
+  eventosFiltrados: Evento[];
+  eventos: Evento[];
+  evento: Evento;
+  modoSalvar = 'post';
+
   imagemLargura = 50;
   imagemMargem = 2;
   mostrarImagem = false;
+  registerForm: FormGroup;
+  bodyDeletarEvento = '';
 
-  constructor(private http: HttpClient) { }
+  // valor digitado
+  vFiltroLista: string;
 
-  ngOnInit() {
-    this.getEventos();
+  get filtroLista(): string {
+    return this.vFiltroLista;
+  }
+  set filtroLista(value: string){
+    this.vFiltroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
   }
 
+  // construtor
+  constructor(
+    private eventoService: EventoService,
+    private modalService: BsModalService,
+    private fb: FormBuilder,
+    private localeService: BsLocaleService ) {
+      this.localeService.use('pt-br');
+    }
+
+    editarEvento(evento: Evento, template: any) {
+      this.modoSalvar = 'put';
+      this.openModal(template);
+      this.evento = evento;
+      this.registerForm.patchValue(evento);
+    }
+
+    novoEvento(template: any){
+      this.modoSalvar = 'post';
+      this.openModal(template);
+    }
+
+    excluirEvento(evento: Evento, template: any) {
+      this.openModal(template);
+      this.evento = evento;
+      this.bodyDeletarEvento = `Tem certeza que deseja excluir o evento: ${evento.tema}, código: ${evento.id}`;
+    }
+
+    // excluirEvento(evento: Evento, template: any) {
+    //   this.openModal(template);
+    //   this.evento = evento;
+    //   this.bodyDeletarEvento = `Tem certeza que deseja excluir o Evento: ${evento.tema}, Código: ${evento.id}`;
+    // }
+
+    confirmeDelete(template: any) {
+      this.eventoService.deleteEvento(this.evento.id).subscribe(
+        () => {
+          template.hide();
+          this.getEventos();
+        }, error => {
+          console.log(error);
+        }
+      );
+    } 
+    
+    // confirmeDelete(template: any) {
+    //   this.eventoService.deleteEvento(this.evento.id).subscribe(
+    //     () => {
+    //       template.hide();
+    //       this.getEventos();
+    //       this.toastr.success('Deletado com Sucesso');
+    //     }, error => {
+    //       this.toastr.error('Erro ao tentar Deletar');
+    //       console.log(error);
+    //     }
+    //   );
+    // }
+
+  // abre a tela modal
+  openModal(template: any) {
+    this.registerForm.reset();
+    template.show(template);
+  }
+
+  // igual ao load ou o init
+  ngOnInit() {
+    this.getEventos();
+    this.validation();
+  }
+
+  // função
+  // recebe um parametro e retorna um array de eventos
+  filtrarEventos(filtrarPor: string): Evento[] {
+    // coloca para maiusculo o valor do parametro
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      evento => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    );
+  }
+
+  // função
   alterarImagem(){
     this.mostrarImagem = !this.mostrarImagem;
   }
 
+  validation() {
+    this.registerForm = this.fb.group(
+      // Objeto
+      {
+        tema: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(50)]],
+        local: ['', Validators.required],
+        dataEvento: ['', Validators.required],
+        imagemURL: ['', Validators.required],
+        qtdPessoas: ['', [Validators.required, Validators.max(12000)]],
+        telefone: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+      }
+    );
+  }
+
+  salvarAlteracao(template: any) {
+    if (this.registerForm.valid){
+      if (this.modoSalvar === 'post') {
+        this.evento = Object.assign({}, this.registerForm.value);
+        this.eventoService.postEvento(this.evento).subscribe(
+          (novoEvento: Evento) => {
+            console.log(novoEvento);
+            template.hide();
+            this.getEventos();
+          }, error => {
+            console.log(error);
+          }
+        );
+      } else {
+        // this.registerForm.value = possui as informações do formulario
+        this.evento = Object.assign({id: this.evento.id}, this.registerForm.value);
+        this.eventoService.putEvento(this.evento).subscribe(
+          () => {
+            template.hide();
+            this.getEventos();
+          }, error => {
+            console.log(error);
+          }
+        );
+      }
+    }
+  }
+
+  // função - acessa o service do evento eventoservice
   getEventos() {
-    this.http.get('http://localhost:5000/api/WeatherForecast').subscribe(response => {
-      this.eventos = response;
+    this.eventoService.getAllEvento().subscribe(
+    (pEventos: Evento[]) => {
+      this.eventos = pEventos;
+      this.eventosFiltrados = this.eventos;
+      console.log(this.eventos);
     }, error => {
       console.log(error);
     });
   }
 }
+
+
+
 
 // eventos: any = [
   //   {
